ba0cc89e0a865980a27ddabd1553ee123c22ee6a
sarnickaa <sarnicka.aleks@gmail.com>
2018-10-29 13:41:48 -0400

    exports syntax for node modules

---
+++ D12_ExportsVsModuleExports/Starter/greet.js
@@ -0,0 +1,19 @@
+// exports = function() {
+//     console.log('Hello')
+// }
+
+// console.log(exports)
+// console.log(module.exports)
+
+//module.exports is what is passed to exports - should point to same object in memory
+//exports changed from empty object to function? NO! a new object is created in memory - the reference is broken when a variable is assigned a value with the = operator
+
+
+//MUTATION - adding a method or property to an object
+// not repointing the variable to another point in memory
+exports.greet = function() {
+    console.log('Hello')
+}
+
+console.log(exports)
+console.log(module.exports)
