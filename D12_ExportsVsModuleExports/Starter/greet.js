c93ec7c0b66f4af1e9a8b8651450ea4b91376ef8
sarnickaa <sarnicka.aleks@gmail.com>
2018-10-29 13:43:05 -0400

    edit syntax

--- D12_ExportsVsModuleExports/Starter/greet.js
+++ D12_ExportsVsModuleExports/Starter/greet.js
@@ -6,6 +6,7 @@
 // console.log(module.exports)
 
 //module.exports is what is passed to exports - should point to same object in memory
+
 //exports changed from empty object to function? NO! a new object is created in memory - the reference is broken when a variable is assigned a value with the = operator
 
 
