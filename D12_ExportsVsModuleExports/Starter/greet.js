abe3af9d6791d60fc407c60d2d74ec54a8d3c391
sarnickaa <sarnicka.aleks@gmail.com>
2018-10-29 13:56:49 -0400

    changed core editof for git to VS code

--- D12_ExportsVsModuleExports/Starter/greet.js
+++ D12_ExportsVsModuleExports/Starter/greet.js
@@ -7,7 +7,7 @@
 
 //module.exports is what is passed to exports - should point to same object in memory
 
-//exports changed from empty object to function? NO! a new object is created in memory - the reference is broken when a variable is assigned a value with the = operator
+//exports changed from empty object to function? NO!! a new object is created in memory - the reference is broken when a variable is assigned a value with the = operator
 
 
 //MUTATION - adding a method or property to an object
